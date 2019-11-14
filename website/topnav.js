dd10264efc0431de7e301f06a658e8e8b16132b2
Richard Morrey <spencerthehalfwit@gmail.com>
2019-11-14 07:17:19 +0000

    smartphone-friendly website

---
+++ website/topnav.js
@@ -0,0 +1,8 @@
+function switchMenu() {
+	var x = document.getElementById("mytopnav");
+	if (x.className === "topnav") {
+		   x.className += " responsive";
+	} else {
+		x.className = "topnav";
+	}
+}
