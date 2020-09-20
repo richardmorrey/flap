3a6764598e56730ac918f5baab343b7d566ffc2a
Richard <spencerthehalfwit@gmail.com>
2020-09-20 17:06:01 +0100

    id token validation

--- webapp/go.mod
+++ webapp/go.mod
@@ -5,6 +5,7 @@ go 1.13
 require (
 	github.com/coreos/go-oidc v2.2.1+incompatible
 	github.com/gorilla/handlers v1.5.1
+	github.com/gorilla/mux v1.8.0
 	github.com/pquerna/cachecontrol v0.0.0-20200819021114-67c6ae64274f // indirect
 	golang.org/x/net v0.0.0-20200904194848-62affa334b73
 	golang.org/x/oauth2 v0.0.0-20200902213428-5d25da1a8d43
--- webapp/main.go
+++ webapp/main.go
@@ -5,10 +5,12 @@ import (
 	"net/http"
 	"encoding/json"
 	"os"
+	"time"
+	//"fmt"
 	oidc "github.com/coreos/go-oidc"
 	"golang.org/x/net/context"
 	//"golang.org/x/oauth2"
-	//"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 )
 
 var (
@@ -16,6 +18,25 @@ var (
 	clientSecret = os.Getenv("GOOGLE_OAUTH2_CLIENT_SECRET")
 )
 
+// middlewareIdToken function validates openid id token
+func middlewareIdToken(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		 	pathParams := mux.Vars(r)
+			if raw, ok := pathParams["token"]; !ok {
+				http.Error(w,"Missing argument: id", http.StatusForbidden)
+			} else {
+				parsed,err:= validateIDToken(raw)
+				if err == nil {
+					log.Printf("Authenticated user %s\n", parsed)
+					next.ServeHTTP(w, r)
+				} else {
+					http.Error(w, "Forbidden", http.StatusForbidden)
+				}
+			}
+		})
+}
+
+// validateIDToken validates and parses a raw openid id token
 func validateIDToken(rawIDToken string) (string,error) {
 	
 	// Create verifier
@@ -49,23 +70,30 @@ func validateIDToken(rawIDToken string) (string,error) {
 	return string(data),nil
 }
 
+// flightHistory returns flight history for specified user 
+func flightHistory(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte("flight history will go here"))
+}
+
 // main is main
 func main() {
-	
-			
-	// Handler for app static content requests
-	appfs := http.FileServer(http.Dir("."))
-	http.Handle("/app/", appfs)
-	
-	// Handler for document download requests
-	docfs := http.FileServer(http.Dir(".."))
-	http.Handle("/doc/",docfs)
 
-	// Handler for website requests
-	websitefs := http.FileServer(http.Dir("../website"))
-	http.Handle("/", websitefs)
+	// Top level router
+	r := mux.NewRouter()
+	
+	// REST API router
+	api := r.PathPrefix("/api/v1").Subrouter()
+	api.HandleFunc("/flighthistory/id/{token}", flightHistory).Methods(http.MethodGet)
+	api.Use(middlewareIdToken)
+
+	// Static content
+	r.PathPrefix("/app/").Handler(http.FileServer(http.Dir(".")))
+	r.PathPrefix("/doc/").Handler(http.FileServer(http.Dir("..")))
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("../website")))
 
 	// Start serving up content
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	srv := &http.Server{Handler:r,Addr:":8080",WriteTimeout: 15 * time.Second,ReadTimeout:  15 * time.Second}
+	log.Fatal(srv.ListenAndServe())
 }
 
