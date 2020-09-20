00f801e514683be266b5fa716dc8d181a6aebedc
Richard <spencerthehalfwit@gmail.com>
2020-09-20 13:07:26 +0100

    web app idtoken parsing

--- webapp/go.mod
+++ webapp/go.mod
@@ -2,4 +2,11 @@ module github.com/richardmorrey/flap/webapp
 
 go 1.13
 
-require github.com/gorilla/handlers v1.5.1
+require (
+	github.com/coreos/go-oidc v2.2.1+incompatible
+	github.com/gorilla/handlers v1.5.1
+	github.com/pquerna/cachecontrol v0.0.0-20200819021114-67c6ae64274f // indirect
+	golang.org/x/net v0.0.0-20200904194848-62affa334b73
+	golang.org/x/oauth2 v0.0.0-20200902213428-5d25da1a8d43
+	gopkg.in/square/go-jose.v2 v2.5.1 // indirect
+)
--- webapp/main.go
+++ webapp/main.go
@@ -3,13 +3,56 @@ package main
 import (
 	"log"
 	"net/http"
+	"encoding/json"
+	"os"
+	oidc "github.com/coreos/go-oidc"
+	"golang.org/x/net/context"
+	//"golang.org/x/oauth2"
 	//"github.com/gorilla/handlers"
-	//"os"
 )
+
+var (
+	clientID     = os.Getenv("GOOGLE_OAUTH2_CLIENT_ID")
+	clientSecret = os.Getenv("GOOGLE_OAUTH2_CLIENT_SECRET")
+)
+
+func validateIDToken(rawIDToken string) (string,error) {
+	
+	// Create verifier
+	ctx := context.Background()
+	provider, err := oidc.NewProvider(ctx, "https://accounts.google.com")
+	if err != nil {
+		return "",err
+	}
+	oidcConfig := &oidc.Config{
+		ClientID: clientID,
+	}
+	verifier := provider.Verifier(oidcConfig)
+
+	// Verify id token
+	idToken, err := verifier.Verify(ctx, rawIDToken)
+	if err != nil {
+		return "",err
+	}
+
+	// Parse token to JSON
+	parsed := new(json.RawMessage)
+	if err := idToken.Claims(parsed); err != nil {
+		return "",err
+	}
+
+	// Render json as string
+	data, err := json.MarshalIndent(parsed, "", "    ")
+	if err != nil {
+		return  "",err
+	}
+	return string(data),nil
+}
 
 // main is main
 func main() {
 	
+			
 	// Handler for app static content requests
 	appfs := http.FileServer(http.Dir("."))
 	http.Handle("/app/", appfs)
