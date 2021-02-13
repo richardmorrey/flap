311cd4a677842c0b7756ca53f5d6bb9cb045a957
Richard <spencerthehalfwit@gmail.com>
2021-02-13 17:14:46 +0000

    history wip

--- webapp/app/history.js
+++ webapp/app/history.js
@@ -15,7 +15,8 @@ function showHistory() {
   }
 
 function renderHistory(text) {
-	$('#world-map').vectorMap({map: 'world_mill',zoomButtons : false, backgroundColor:'white',regionStyle: { initial: { fill: '#dc3545' }, hover: { fill: 'black' } }});
+	$('#world-map').vectorMap({map: 'world_mill',zoomButtons : false, backgroundColor:'white',regionStyle: { initial: { fill: '#dc3545' }, hover: { fill: 'black' }} , 
+					markerStyle:{initial: {fill: 'black',stroke: 'white',"fill-opacity": 1,"stroke-width": 1,"stroke-opacity": 1,r: 5}}});
 	$('#world-map').vectorMap('get', 'mapObject').updateSize();
 	populateBoard(text)
         $('#flightList a').on('click', function (e) {
@@ -63,9 +64,21 @@ function populateBoard(text) {
 
 function showFlightPath(i) {
 	cf = flights[i]
-	var markers = [{latLng:[gAirports[cf.From].Lat,gAirports[cf.From].Lng],name:gAirports[cf.From].Nm },
-			{latLng:[gAirports[cf.To].Lat,gAirports[cf.To].Lng],name:gAirports[cf.To].Nm }]
+	nDots= Math.min(cf.Distance/100,10)
+	from = [gAirports[cf.From].Lat, gAirports[cf.From].Lng]
+	to = [gAirports[cf.To].Lat, gAirports[cf.To].Lng]
+	var latdelta=(to[0] - from[0])/(nDots-1)
+	var lngdelta=(to[1] - from[1])/(nDots-1)
+	var markers = []
+	var lat=from[0]
+	var lng=from[1]
+	for (var i=0;i < nDots; i++) {
+		markers.push({latLng:[lat,lng]})
+		lat += latdelta
+		lng += lngdelta
+	}
 	var map = $('#world-map').vectorMap('get', 'mapObject')
 	map.removeAllMarkers()
 	map.addMarkers(markers)
+	map.setFocus({scale:20000/cf.Distance, lat:from[0]+(to[0]-from[0])/2, lng:from[1]+(to[1]-from[1])/2, animate:true})
 }
