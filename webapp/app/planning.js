221f92a84579d9635a5a1f25bcb366c17e596b3a
Richard <spencerthehalfwit@gmail.com>
2021-02-28 17:47:02 +0000

    planning wip

--- webapp/app/planning.js
+++ webapp/app/planning.js
@@ -5,7 +5,7 @@ function showPlanning() {
 	  var user = GoogleAuth.currentUser.get();
 	  var id_token = user.getAuthResponse().id_token;
 	  var xhr = new XMLHttpRequest();
-	  xhr.open('GET', '/user/v1/flighthistory/id/'+id_token+"/b/5/n/1");
+	  xhr.open('GET', '/user/v1/promises/id/'+id_token+"/b/5/n/1");
 	  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 	  xhr.onload = function() {
 				   renderPlanning(xhr.responseText);
@@ -16,9 +16,37 @@ function showPlanning() {
        }
   }
 
+var events=[]
 function renderPlanning(text) {
-	new Calendar('#calendar')
+	var promises = JSON.parse(text)
+alert(text)	
+	var stacksize=0
+	for (i in promises) {
+		if (promises[i].Stacked == 0) {
+			for (y=i; y >= Math.max(0, i-stacksize);y--) {
+			    promises[y].level=stacksize
+			}
+			stacksize=0
+		}
+	}
+
+	for (i in promises)  {
+		events.push({level:promises[i].level,startDate: new Date(promises[i].TripStart.substr(0,10)), endDate: new Date(promises[i].TripEnd.substr(0,10))})
+	}
+
+	var cal = new Calendar('#calendar',{
+		dataSource: events,
+		customDataSourceRenderer: dsRender,
+		style: "custom"
+		})
 	navbarActive('planning');
 	planningInit=true
+}
+
+function dsRender(element, date, events) {
+	var bgs=['grey','dimgrey','black']
+	$(element).css('background-color', bgs[events[0].level]);
+	$(element).css('color',"white");
+        $(element).css('border-radius', '0px');
 }
 
